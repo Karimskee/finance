69123b9ab0a05140aae84fd0ec7b6096be5f6799
Karimskee <kareemgamer2006@gmail.com>
2026-04-29 19:45:22 +0300

    docs: clarified the running method

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# USE "flask run" COMMAND TO RUN THE APPLICATION
+
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
